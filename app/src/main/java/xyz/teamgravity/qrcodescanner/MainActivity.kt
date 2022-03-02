f0680d912eeb3a107c6fdafceccaa6abb700199f
raheemadamboev <vividricemaryandmax@gmail.com>
2022-03-02 16:54:14 +0500

    handling permissions, and integrating cameraX to compose also reading and decoding qr code

--- app/src/main/java/xyz/teamgravity/qrcodescanner/MainActivity.kt
+++ app/src/main/java/xyz/teamgravity/qrcodescanner/MainActivity.kt
@@ -1,15 +1,33 @@
 package xyz.teamgravity.qrcodescanner
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.ContextCompat
 import xyz.teamgravity.qrcodescanner.ui.theme.QRCodeScannerTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,6 +37,75 @@ class MainActivity : ComponentActivity() {
             QRCodeScannerTheme {
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
 
+                    val context = LocalContext.current
+                    val lifecycleOwner = LocalLifecycleOwner.current
+                    val cameraProviderFuture = remember { ProcessCameraProvider.getInstance(context) }
+
+                    var code by remember { mutableStateOf("") }
+                    var hasCameraPermission by remember {
+                        mutableStateOf(
+                            ContextCompat.checkSelfPermission(
+                                context,
+                                Manifest.permission.CAMERA
+                            ) == PackageManager.PERMISSION_GRANTED
+                        )
+                    }
+                    val launcher = rememberLauncherForActivityResult(
+                        contract = ActivityResultContracts.RequestPermission(),
+                        onResult = { granted ->
+                            hasCameraPermission = granted
+                        }
+                    )
+
+                    LaunchedEffect(key1 = true) {
+                        launcher.launch(Manifest.permission.CAMERA)
+                    }
+
+                    Column(modifier = Modifier.fillMaxSize()) {
+                        if (hasCameraPermission) {
+                            AndroidView(
+                                factory = { context ->
+                                    val previewView = PreviewView(context)
+                                    val preview = Preview.Builder().build()
+                                    val selector = CameraSelector.Builder()
+                                        .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                                        .build()
+                                    preview.setSurfaceProvider(previewView.surfaceProvider)
+                                    val imageAnalysis = ImageAnalysis.Builder()
+                                        .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                                        .build()
+                                    imageAnalysis.setAnalyzer(
+                                        ContextCompat.getMainExecutor(context),
+                                        QRCodeAnalyzer { result ->
+                                            result?.let { code = it }
+                                        }
+                                    )
+
+                                    try {
+                                        cameraProviderFuture.get().bindToLifecycle(
+                                            lifecycleOwner,
+                                            selector,
+                                            preview,
+                                            imageAnalysis
+                                        )
+                                    } catch (e: Exception) {
+                                        e.printStackTrace()
+                                    }
+
+                                    return@AndroidView previewView
+                                },
+                                modifier = Modifier.weight(1f)
+                            )
+                            Text(
+                                text = code,
+                                fontSize = 20.sp,
+                                fontWeight = FontWeight.Bold,
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(32.dp)
+                            )
+                        }
+                    }
                 }
             }
         }
